1fc9ee44cdf76ce38dd5d9c6c7cc7c1907923999
LuisFerOv25 <ingluisfer25@udenar.edu.co>
2022-07-27 14:21:24 -0500

    Definicion de componentes y rutas en Angular

--- src/app/Shared/shared/shared-routing.module.ts
+++ src/app/Shared/shared/shared-routing.module.ts
@@ -1,6 +1,5 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginPageComponent } from 'src/app/Modules/auth/login-page/login-page.component';
 import { RegisterComponent } from 'src/app/Modules/auth/register/register.component';
 import { ValidationComponent } from 'src/app/Modules/auth/validation/validation.component';
@@ -10,54 +9,49 @@ import { FooterComponent } from 'src/app/Modules/home/footer/footer.component';
 import { HeadersComponent } from 'src/app/Modules/home/headers/headers.component';
 import { TracklistComponent } from 'src/app/Modules/track/tracklist/tracklist.component';
 
-
 const routes: Routes = [
 
-  {
-    path: '', component: AppComponent
-
-  },
   //RUTAS DEL MODULO AUTH
   {
-    path: 'login', component: LoginPageComponent
+    path: 'login', component:LoginPageComponent
     //crear un componente llamado login-page dentro del medio Auth
   },
   {
-    path: 'registrer', component: RegisterComponent
+    path: 'registrer', component:RegisterComponent
 
   },
   {
-    path: 'validation', component: ValidationComponent
-
+    path: 'validation', component:ValidationComponent
+ 
   },
   //RUTAS DEL MODULO HOME
   {
-    path: 'headers', component: HeadersComponent
-
+    path: 'headers', component:HeadersComponent
+   
   },
   {
-    path: 'footer', component: FooterComponent
-
+    path: 'footer', component:FooterComponent
+   
   },
-  //RUTAS DEL MODULO TRACKS
+   //RUTAS DEL MODULO TRACKS
   {
-    path: 'tracklist', component: TracklistComponent
-
-  },
-  //RUTAS DEL MODULO HISTORY
+    path: 'tracklist', component:TracklistComponent
+   
+  },  
+   //RUTAS DEL MODULO HISTORY
   {
-    path: 'historymenu', component: HistorymenuComponent
-
-  },
-  //RUTAS DEL MODULO FAVORITOS
-  {
-    path: 'marcadoresview', component: MarcadoresViewComponent
-
-  },
+    path: 'historymenu', component:HistorymenuComponent
+    
+  },   
+   //RUTAS DEL MODULO FAVORITOS
+   {
+    path: 'marcadoresview', component:MarcadoresViewComponent
+   
+  }, 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class SharedRoutingModule { }
